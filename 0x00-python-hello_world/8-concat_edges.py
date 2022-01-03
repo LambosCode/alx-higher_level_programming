932fa814a576591821ca53788628de373d7b02ec
LambosCode <hameedsleague@gmail.com>
2022-01-03 22:01:57 +0000

    fix errors

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str + " object-oriented programming with Python"
+str = str[130:]
+print(str)
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,2 @@
+This is start off seeing how good I can be with python.
+And carry on with C in data structures. Particularly linked list.
