722225759aa5e879ef9f0274518a923313d723c0
LambosCode <hameedsleague@gmail.com>
2022-01-08 00:08:14 +0000

    nw crct

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
 if len(argv) > 4 or len(argv) < 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit()
