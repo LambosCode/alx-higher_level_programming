e7d8fa6aa52b954e42016d778701225665331b73
LambosCode <hameedsleague@gmail.com>
2022-01-10 14:34:19 +0000

    nw subs

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    ln = 0
+    lnth = len(my_string)
+    while ln < lnth:
+        if my_string[ln] == 'c':
+            my_string[ln] = ' '
+        ln = ln + 1
+    return my_school
