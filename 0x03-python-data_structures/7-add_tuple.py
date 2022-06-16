06530a92c0bfa5092f31286ea57af773f5585f0a
LambosCode <hameedsleague@gmail.com>
2022-06-16 22:40:25 +0100

    works

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     summ = list()
     cnt = 0
     while cnt < 2:
+        if type(tuple_a) is int or type(tuple_b) is int:
+            summ.append(tuple_a + tuple_b)
+            cnt += 1
+            continue
         if cnt >= len(tuple_a):
             summ.append(tuple_b[cnt])
             cnt += 1
@@ -10,11 +14,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             summ.append(tuple_a[cnt])
             cnt += 1
         else:
-            if tuple_a is None or tuple_b is None:
-                summ.append(0)
-                cmt += 1
-            else:
-                summ.append(tuple_a[cnt] + tuple_b[cnt])
-                cnt += 1
+            summ.append(tuple_a[cnt] + tuple_b[cnt])
+            cnt += 1
     sumd = tuple(summ)
     return sumd
